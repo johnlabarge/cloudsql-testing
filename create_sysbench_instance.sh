3141a3d690310f03f1b468a0a5df305158ad8498
Phillip Knoll <phillip@phkn.net>
2019-06-03 20:21:03 -0500

    Refactor: collapse sysbench init script to committed version (since it doesn't contain any config variables)

--- create_sysbench_instance.sh
+++ create_sysbench_instance.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 source env.sh
-cat <<EOF >sysbench_init.sh
-#!/bin/sh
-apt-get update
-apt-get install -y xfsprogs sysstat git iotop htop automake libtool libssl-dev make automake default-libmysqlclient-dev mysql-client
-echo "installing Percona"
-apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 1C4CBDCDCD2EFD2A
-echo "deb http://repo.percona.com/apt \$(lsb_release -cs) main" > /etc/apt/sources.list.d/percona.list
-apt-get update
-apt-get install -y   percona-server-client-5.7  libperconaserverclient20-dev  
-cd /opt
-git clone https://github.com/Percona-Lab/sysbench.git
-chmod -R 755 sysbench
-cd sysbench
-./autogen.sh
-./configure
-make install
-cp -r sysbench/tests /usr/share/doc/sysbench/
-mkdir -p /usr/share/doc/sysbench
-chgrp -R adm /usr/share
-chmod 775 -R /usr/share/doc/sysbench
-EOF
 
 gcloud compute instances create $SYSBENCH_INSTANCE --zone=$SYSBENCH_ZONE --metadata-from-file startup-script=sysbench_init.sh
 
--- sysbench_init.sh
+++ sysbench_init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 apt-get update
-apt-get install -y xfsprogs sysstat git iotop htop automake libtool libssl-dev make automake default-libmysqlclient-dev
+apt-get install -y xfsprogs sysstat git iotop htop automake libtool libssl-dev make automake default-libmysqlclient-dev mysql-client
 echo "installing Percona"
 apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 1C4CBDCDCD2EFD2A
 echo "deb http://repo.percona.com/apt $(lsb_release -cs) main" > /etc/apt/sources.list.d/percona.list
 apt-get update
-apt-get install -y   percona-server-client-5.7  libperconaserverclient20-dev
+apt-get install -y   percona-server-client-5.7  libperconaserverclient20-dev  
 cd /opt
 git clone https://github.com/Percona-Lab/sysbench.git
 chmod -R 755 sysbench
@@ -13,6 +13,7 @@ cd sysbench
 ./autogen.sh
 ./configure
 make install
+cp -r sysbench/tests /usr/share/doc/sysbench/
 mkdir -p /usr/share/doc/sysbench
 chgrp -R adm /usr/share
 chmod 775 -R /usr/share/doc/sysbench
